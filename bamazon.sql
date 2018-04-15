6589b96b04d8e37298c6c2e23fc65ad56e16bd27
Ryan <rjlafazan@gmail.com>
2018-04-15 14:58:18 -0700

    all table columns set to not null

--- bamazon.sql
+++ bamazon.sql
@@ -6,13 +6,13 @@ CREATE TABLE products
     item_id INT(100)
     AUTO_INCREMENT NOT NULL,
   product_name VARCHAR
-    (100) NULL,
-  department_name VARCHAR
-    (100) NULL,
-  price DECIMAL
-    (10,2) INT NULL,
-  stock_quantity INT
     (100) NOT NULL,
+  department_name VARCHAR
+    (100) NOT NULL,
+  price DECIMAL
+    (10,2) INT NOT NULL,
+  stock_quantity INT
+    (100) NOT NOT NULL,
   PRIMARY KEY
     (item_id)
 );
