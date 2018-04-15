54bfa3be394034a607c31f080465f25046dd1106
Ryan <rjlafazan@gmail.com>
2018-04-15 14:56:43 -0700

    bamazon sql initial

---
+++ bamazon.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+USE bamazonDB;
+CREATE TABLE products
+(
+    item_id INT(100)
+    AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR
+    (100) NULL,
+  department_name VARCHAR
+    (100) NULL,
+  price DECIMAL
+    (10,2) INT NULL,
+  stock_quantity INT
+    (100) NOT NULL,
+  PRIMARY KEY
+    (item_id)
+);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("T-shirt", "Clothing", 20, 25);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Cat Picture - limited edition", "Art", 1000, 4);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Cat Picture - standard", "Art", 15, 98);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Dog Playtime", "Service", 25, 3);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Good Cheese", "Food", 99, 30);
+    SELECT *
+    FROM products;
