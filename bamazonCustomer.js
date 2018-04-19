5416a5e3fc381b31abec25e95e2006f743fb22c9
Ryan <rjlafazan@gmail.com>
2018-04-19 14:35:06 -0700

    customer js works now... time to buy some of that Good Cheese

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "root",
-  database: "bamazon",
+  database: "bamazonDB",
 });
 
 connection.connect(function(err) {
